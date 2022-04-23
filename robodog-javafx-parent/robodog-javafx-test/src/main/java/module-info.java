a0aabe7341e83ed6484e440c8032b76a6c2314eb
carddamom <carddamom@tutanota.com>
2022-04-23 19:33:46 +0100

    feat: Completed the javafx-processor

---
+++ robodog-javafx-parent/robodog-javafx-test/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+open module robodog.javafx.test {
+  requires javafx.graphics;
+  requires javafx.fxml;
+  requires javax.inject;
+  requires javafx.base;
+  requires javafx.controls;
+  requires static robodog.javafx.annotations;
+  requires java.compiler;
+}
